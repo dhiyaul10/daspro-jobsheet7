6916ef19a6f9573446f83b09a9c493d32001ee36
dhiyaul10 <144310130+dhiyaul10@users.noreply.github.com>
2023-10-16 12:23:58 +0700

    Add files via upload

---
+++ ForKelipatan08.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ForKelipatan08 {
+    public static void main(String[] args) {
+
+        Scanner input08 = new Scanner(System.in);
+        int kelipatan;
+        int jumlah = 0;
+        int counter = 0;
+
+        System.out.print("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = input08.nextInt();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+        }
+
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+    }
+}
